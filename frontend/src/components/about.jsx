00a20157e8804c74a0be5594fd020dda32f7c3c8
Raghavendra Kotnis <raghavendra_kotnis05@icloud.com>
2024-03-31 15:44:35 +0530

    welcome page

---
+++ frontend/src/components/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./about.css"
+
+function About(){
+    return (
+        <div className='about'>
+            <div className='info'>
+            <p>
+            Connect, Code, Collaborate: 
+            </p>
+            <p className='text'>
+            Your Multiplayer Editing Playground
+            </p>
+            </div>
+            <div className='code_img'></div>
+        </div>
+    )
+}
+export default About;
--- frontend/src/components/welcome.jsx
+++ frontend/src/components/welcome.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Header from './header';
+import About from './about';
+import Team from './team';
+
 export default function Welcome() {
-  return <div className="bg-red-500 ">welcome page</div>;
+  return <div className="welcome">
+    <Header />
+    <About />
+    <Team />
+  </div>;
 }
